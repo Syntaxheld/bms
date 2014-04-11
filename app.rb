55acc3872634ea8613b138f72d87045dc8708a53
Stefan <sherkt@ebay.com>
2014-04-11 16:13:51 +0200

    condense /card/add & implement /transaction/pay

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ require "json"
 config = YAML.load_file("config.yml")[settings.environment.to_s]
 Braintree::Configuration.environment = config["braintree_environment"].to_sym
 Braintree::Configuration.merchant_id = config["merchant_id"]
-Braintree::Configuration.public_key = config["public_key"]
+Braintree::Configuration.public_key  = config["public_key"]
 Braintree::Configuration.private_key = config["private_key"]
 CSE_KEY = config['cse_key']
 
@@ -16,17 +16,12 @@ end
 
 post "/card/add" do
   result = Braintree::Customer.create({
+    :id => params[:email],
     "credit_card" => {
       "number" => params[:card_number],
       "cvv" => params[:cvv],
       "expiration_month" => params[:expiration_month],
       "expiration_year" => params[:expiration_year],
-      "billing_address" => {
-        "postal_code" => params[:zipcode],
-      },
-      "options" => {
-        "venmo_sdk_session" => params[:venmo_sdk_session],
-      },
     },
   })
 
@@ -35,31 +30,25 @@ post "/card/add" do
   if !result.success?
     out["error"] = result.message
     halt(422, JSON.generate(out))
-  elsif !result.customer.credit_cards[0].venmo_sdk?
-    out["error"] = "Card saved to Braintree vault, but could not be saved to Venmo Touch"
-    halt(422, JSON.generate(out))
   else
-    out["payment_method_token"] = result.customer.credit_cards[0].token
+    # out["payment_method_token"] = result.customer.credit_cards[0].token
     return JSON.generate(out)
   end
 end
 
-post "/card/payment_method_code" do
-  result = Braintree::Customer.create({
-    "credit_card" => {
-      "venmo_sdk_payment_method_code" => params[:payment_method_code],
-    },
-  })
+post "/transaction/pay" do
+  result = Braintree::Transaction.sale(
+    :amount => params[:amount],
+    :customer_id => params[:email],
+  )
+  
   out = {"success" => result.success?, "error" => nil}
-
+  
   if !result.success?
     out["error"] = result.message
     halt(422, JSON.generate(out))
-  elsif !result.customer.credit_cards[0].venmo_sdk?
-    out["error"] = "Card used successfully, but was not marked as Venmo Touch related"
-    halt(422, JSON.generate(out))
   else
-    out["payment_method_token"] = result.customer.credit_cards[0].token
+    out["currency_iso_code"] = result.transaction.currency_iso_code
     return JSON.generate(out)
   end
 end
